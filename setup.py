9887fdd0fc85741b4c2b638309fd092c280027d0
rfarley <rfarley@mitre.org>
2017-03-23 20:04:19 -0400

    Add package skel

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='lcd_restful',
+    packages=['lcd_restful'],
+    version='0.0.1',
+    description=(
+        "RESTful controller for HD44780 Character Panel LCD " +
+        "on the Raspberry Pi. Supports testing on non-Rpi systems"),
+    author='Ryan Farley',
+    author_email='rfarley3@gmu.edu',
+    # url='https://github.com/rfarley3/lcd_restful',
+    # download_url='https://github.com/rfarley3/radio/tarball/0.1.1',
+    keywords=['lcd', 'rpi', 'hd44780'],
+    classifiers=[],
+    install_requires=[
+        'Adafruit_CharLCD',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lcd_server = lcd_restful.__main__:main_serv',
+        ]
+    },
+    include_package_data=True,
+)
