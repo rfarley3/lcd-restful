fe394cd7a1cc29a7fa611501b85af7702630c3af
rfarley <rfarley@mitre.org>
2017-04-15 21:14:27 -0400

    Auto-rollback to term if not on a RPi, add interact to pause test screens

--- lcd-test.py
+++ lcd-test.py
@@ -3,26 +3,44 @@
 import sys
 import time
 from lcd_restful.lcd import Lcd
-# from lcd_restful.fake import FakeLcdApi as Lcd
+from lcd_restful.fake import FakeLcdApi as FakeLcd
 
 # WARNING double check pin configuration in lcd_restful.lcd
 
+def pause(lcd, interact=False):
+    if interact:
+        input('Press enter for next test')
+        print('\033[A', end='')
+    else:
+        time.sleep(2.0)
+    lcd.clear()
 
 def main(argv):
+    interact = False
+    if '-p' in argv or '--prompt' in argv:
+        interact = True
+    use_fake = False
+    if '-f' in argv or '--fake' in argv:
+        use_fake = True
+    if not use_fake:
+        try:
+            lcd = Lcd()
+        except FileNotFoundError:  # system detection is via /proc/cpuinfo
+            print('Warning, this is not an RPi system, rolling back to fake LCD')
+            use_fake = True
+    if use_fake:
+        lcd = FakeLcd()
     # from lcd_restful.lcd import HITACHI_CHAR_MAP
     # print('Char map is %s' % len(HITACHI_CHAR_MAP))
     # for i, ch in enumerate(HITACHI_CHAR_MAP):
     #     print('%s: %s' % (i, ch))
-    lcd = Lcd()
     lcd.message('Hello\nworld!')
-    time.sleep(1.0)
-    lcd.clear()
+    pause(lcd, interact)
     lcd.message('1' * 20 + '\n' + '2' * 20 + '\n' + '3' * 20 + '\n' + '4' * 20)
-    time.sleep(1.0)
+    pause(lcd, interact)
     # Show all possible characters on display
     i = 0
     while i < 256:
-        lcd.clear()
         lines = []
         for j in range(4):  # lcd.rows
             line = ''
@@ -33,16 +51,13 @@ def main(argv):
                 i += 1
             lines.append(line)
         lcd.message(lines, as_ordinal=True)
-        time.sleep(4.0)
-    lcd.clear()
+        pause(lcd, interact)
     lcd.message('Testing message that needs autowrap', autowrap=True)
-    time.sleep(1.0)
-    lcd.clear()
+    pause(lcd, interact)
     lcd.message('Testing\nmessage with\ntoo many\nlines\nshould not see me', autowrap=True)
-    time.sleep(1.0)
-    lcd.clear()
+    pause(lcd, interact)
     lcd.message('Testing message by\nutf8 strings')
-    time.sleep(1.0)
+    pause(lcd, interact)
     test_lines = [
         (' !"#$%&\'()*+,-./\n' +
          '0123456789:;<=>?\n' +
@@ -57,28 +72,25 @@ def main(argv):
         ('αäβεμσρg√⁻jˣ¢£ñö\n' +
          'pqθ∞ΩüΣπxy千万円÷ █')]
     for l in test_lines:
-        lcd.clear()
         lcd.message(l)
-        time.sleep(8.0)
+        pause(lcd, interact)
 
     # # Demo showing the cursor.
-    # lcd.clear()
     # lcd.show_cursor(True)
     # lcd.message('Show cursor')
-    # time.sleep(2.0)
+    # pause(lcd, interact)
 
     # # Demo showing the blinking cursor.
-    # lcd.clear()
     # lcd.blink(True)
     # lcd.message('Blink cursor')
-    # time.sleep(2.0)
+    # pause(lcd, interact)
 
     # # Stop blinking and showing cursor.
     # lcd.show_cursor(False)
     # lcd.blink(False)
+    # pause(lcd, interact)
 
     # Demo scrolling message right/left.
-    lcd.clear()
     message = 'Scroll'
     lcd.message(message)
     for i in range(lcd._cols - len(message)):
@@ -87,12 +99,10 @@ def main(argv):
     for i in range(lcd._cols - len(message)):
         time.sleep(0.5)
         lcd.move_left()
-    time.sleep(0.5)
+    pause(lcd, interact)
 
-    lcd.clear()
     lcd.message('Goodbye!')
-    time.sleep(2.0)
-    lcd.clear()
+    pause(lcd, False)
     return 0
 
 
